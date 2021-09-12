fda32f4de0f3532d0aa0134c490ea77fc9c314d8
makar <mmk@liis.su>
2021-09-12 16:16:03 +0300

    add products1

---
+++ client/src/components/products1/Products1.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import {
+  drinks,
+  recipe,
+  conservants,
+  factory,
+  hands,
+  nature,
+  storages,
+  time,
+} from './svgs'
+
+function Products1() {
+  return (
+    <div className="products1">
+      <h3>ПРЕИМУЩЕСТВА</h3>
+      <div className="products1__items-wrapper">
+        <div className="products1__item">
+          {drinks()}
+          <span>изготовление лимонадов и сиропов</span>
+        </div>
+        <div className="products1__item">
+          {recipe()}
+          <span>уникальная рецептура основателя </span>
+        </div>
+        <div className="products1__item">
+          {conservants()}
+          <span>пищевые добавки отсутствуют в составе</span>
+        </div>
+        <div className="products1__item">
+          {nature()}
+          <span>экологически безопасная упаковка товара</span>
+        </div>
+        <div className="products1__item">
+          {time()}
+          <span>проверенная столетним опытом работы марка</span>
+        </div>
+        <div className="products1__item">
+          {factory()}
+          <span>производство на территории Ленинградской обл.</span>
+        </div>
+        <div className="products1__item">
+          {storages()}
+          <span>длительный срок хранения продукции</span>
+        </div>
+        <div className="products1__item">
+          {hands()}
+          <span>отлаженная система сотрудничества</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Products1
--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '@components/header/Header'
+import Products1 from '@/components/products1/Products1'
 
 function Home() {
   return (
     <div>
       <Header />
+      <Products1/>
     </div>
   )
 }
